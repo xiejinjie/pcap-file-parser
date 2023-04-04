372c1cd0f511e372075fda6fa4b997ec3b1fceec
xiejinjie <544482194@qq.com>
2023-04-04 13:55:21 +0800

    parse pcap file

--- src/main/java/top/kcoder/constant/Constant.java
+++ src/main/java/top/kcoder/constant/Constant.java
@@ -8,20 +8,7 @@ public class Constant {
 
     public static final int TYPE_VLAN = 0x8100;
     public static final int TYPE_IPV6 = 0x86dd;
-    public static final int VERSION_IPV6 = 6;
 
-    public static final int HEADER_IDP = 0x92;
-
-    public static final int HEADER_SEAGP = 0x93;
-
-    public static final int HEADER_SEASP = 3;
-
-    public static final int FRAME_TYPE_STREAM_MAX = 0x7;
-
-    /*
-     * 计算标识
-     */
-    public static final int SIGN_HIDDEN = 0x80;
 
     public static Protocol[][] PROCESS_PIPELINE = {
             {Ethernet, LinuxSll},
